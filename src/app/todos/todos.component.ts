69012e97e20b3da92dc96b7be2f6f61cfee79292
Chukwu Sydney <chukwusydney061@gmail.com>
2025-04-27 23:12:57 +0100

    Add Angular Pipes

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -4,17 +4,20 @@ import { Todo } from '../model/todo.type';
 import { catchError } from 'rxjs';
 import { NgIf } from '@angular/common';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
   standalone:true,
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.css'
 })
 export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     this.todoService.getTodosFromApi().pipe(
